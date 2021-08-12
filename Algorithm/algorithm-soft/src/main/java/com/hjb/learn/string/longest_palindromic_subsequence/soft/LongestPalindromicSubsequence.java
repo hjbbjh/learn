93de2fbd3edfd54c94a4e440b797fcd97b0a8bcb
haojingbin <haojingbin@cmss.chinamobile.com>
2021-08-12 10:08:53 +0800

    字符串相关

---
+++ Algorithm/algorithm-soft/src/main/java/com/hjb/learn/string/longest_palindromic_subsequence/soft/LongestPalindromicSubsequence.java
@@ -0,0 +1,39 @@
+package com.hjb.learn.string.longest_palindromic_subsequence.soft;
+
+/**
+ * ClassName: LongestPalindromicSubsequence
+ * Description: 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * <p>
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "bbbab"
+ * 输出：4
+ * 解释：一个可能的最长回文子序列为 "bbbb" 。
+ * 示例 2：
+ * <p>
+ * 输入：s = "cbbd"
+ * 输出：2
+ * 解释：一个可能的最长回文子序列为 "bb" 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by haojingbin on 2021/8/12 8:27
+ *
+ * @author haojingbin
+ */
+public class LongestPalindromicSubsequence {
+
+    public static int longestPalindromeSubseq(String s) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
