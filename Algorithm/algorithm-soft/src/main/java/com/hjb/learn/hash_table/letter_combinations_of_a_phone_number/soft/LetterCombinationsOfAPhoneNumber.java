e63ed9b8f39b28ac15b133d6e3ffc61c31cd1a2d
haojingbin <haojingbin@cmss.chinamobile.com>
2021-08-23 16:22:23 +0800

    hash table and sort

---
+++ Algorithm/algorithm-soft/src/main/java/com/hjb/learn/hash_table/letter_combinations_of_a_phone_number/soft/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,38 @@
+package com.hjb.learn.hash_table.letter_combinations_of_a_phone_number.soft;
+
+/**
+ * ClassName: LetterCombinationsOfAPhoneNumber
+ * Description: 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ * <p>
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * <p>
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：digits = "23"
+ * 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * 示例 2：
+ * <p>
+ * 输入：digits = ""
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：digits = "2"
+ * 输出：["a","b","c"]
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by haojingbin on 2021/8/23 10:40
+ *
+ * @author haojingbin
+ */
+public class LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Algorithm/algorithm-soft/src/main/java/com/hjb/learn/sort/group_anagrams/soft/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package com.hjb.learn.sort.group_anagrams.soft;
+
+/**
+ * ClassName: GroupAnagrams
+ * Description: 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * <p>
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母都恰好只用一次。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ * <p>
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * <p>
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by haojingbin on 2021/8/23 10:42
+ *
+ * @author haojingbin
+ */
+public class GroupAnagrams {
+    public static void main(String[] args) {
+
+    }
+}
